b808183f0ef91c94ec3094d7c4775fc0ea9a4fb1
Anwesh43 <anweshthecool0@gmail.com>
2020-09-18 18:53:45 +0530

    adding view in activity and fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkeddivideinmiddleballview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkeddivideinmiddleballview/MainActivity.kt
@@ -2,11 +2,20 @@ package com.anwesh.uiprojects.linkeddivideinmiddleballview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.dividemiddleballview.DivideMiddleBallView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        DivideMiddleBallView.create(this)
+        fullScreen()
     }
+}
+
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }

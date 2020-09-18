1a77624346e680de214138c64ed52e3860e1f1a4
Anwesh43 <anweshthecool0@gmail.com>
2020-09-18 17:37:38 +0530

    adding Renderer class

--- dividemiddleballview/src/main/java/com/anwesh/uiprojects/dividemiddleballview/DivideMiddleBallView.kt
+++ dividemiddleballview/src/main/java/com/anwesh/uiprojects/dividemiddleballview/DivideMiddleBallView.kt
@@ -188,4 +188,27 @@ class DivideMiddleBallView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : DivideMiddleBallView) {
+
+        private val animator : Animator = Animator(view)
+        private val dmb : DivideMiddleBall = DivideMiddleBall(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(foreColor)
+            dmb.draw(canvas, paint)
+            animator.animate {
+                dmb.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            dmb.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }

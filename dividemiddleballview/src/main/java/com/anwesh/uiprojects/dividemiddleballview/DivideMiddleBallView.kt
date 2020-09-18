3782c93924e8403c973c276726725fef379092e7
Anwesh43 <anweshthecool0@gmail.com>
2020-09-18 14:19:13 +0530

    adding constants

--- dividemiddleballview/src/main/java/com/anwesh/uiprojects/dividemiddleballview/DivideMiddleBallView.kt
+++ dividemiddleballview/src/main/java/com/anwesh/uiprojects/dividemiddleballview/DivideMiddleBallView.kt
@@ -4,3 +4,26 @@ package com.anwesh.uiprojects.dividemiddleballview
  * Created by anweshmishra on 18/09/20.
  */
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val parts : Int = 3
+val colors : Array<Int> = arrayOf(
+        "#3F51B5",
+        "#03A9F4",
+        "#4CAF50",
+        "#FF5722",
+        "#FFEB3B"
+).map({Color.parseColor(it)}).toTypedArray()
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val rFactor : Float = 14.2f
+val foreColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
+val xFactor : Float = 0.35f
+

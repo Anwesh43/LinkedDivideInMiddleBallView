2bc730bd3627e3a930591fd06382bb704a4dc005
Anwesh43 <anweshthecool0@gmail.com>
2020-09-18 15:50:50 +0530

    adding Canvas extension methods

--- dividemiddleballview/src/main/java/com/anwesh/uiprojects/dividemiddleballview/DivideMiddleBallView.kt
+++ dividemiddleballview/src/main/java/com/anwesh/uiprojects/dividemiddleballview/DivideMiddleBallView.kt
@@ -12,7 +12,7 @@ import android.graphics.Color
 import android.app.Activity
 import android.content.Context
 
-val parts : Int = 3
+val parts : Int = 5
 val colors : Array<Int> = arrayOf(
         "#3F51B5",
         "#03A9F4",
@@ -26,8 +26,38 @@ val rFactor : Float = 14.2f
 val foreColor : Int = Color.parseColor("#BDBDBD")
 val delay : Long = 20
 val xFactor : Float = 0.35f
+val yFactor : Float = 0.3f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawDivideBallView(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val sc5 : Float = scale.divideScale(4, parts)
+    val r : Float = Math.min(w, h) / rFactor
+    val currX : Float = (w - 2 * r) * xFactor
+    val wLeft : Float = w - 2 * currX
+    val x : Float = currX * sc2 + (wLeft) * sc3 + currX * sc4
+    val y : Float = yFactor * h * sc3.sinify()
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        save()
+        scale(1f, 1f - 2 * j)
+        drawCircle(x, y, r * (sc1 - sc5), paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawDMBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    drawDivideBallView(scale, w, h, paint)
+}
